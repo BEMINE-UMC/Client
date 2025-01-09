70e8b045f3572d91237fa90c8e77299288c664af
nayeong lee <lny021102@gmail.com>
2025-01-10 02:24:02 +0900

    feat: Header 컴포넌트 완성, 전역 스타일링 수정

--- src/pages/mine/MyPage.tsx
+++ src/pages/mine/MyPage.tsx
@@ -1,10 +1,12 @@
-
+// 마이페이지 메인 UI, 나영 작업 공간
+import CustomColumn from "./components/CustomColumn";
+import CustomFont from "./components/CustomFont";
 
 const MyPage = () => {
     return (
-        <div>
-
-        </div>
+        <CustomColumn $width="100vw" $minHeight="100vh" $alignitems="center" $justifycontent="center">
+            <CustomFont $color="black" $font="1rem">여기는 마이페이지</CustomFont>
+        </CustomColumn>
     );
 }
 
