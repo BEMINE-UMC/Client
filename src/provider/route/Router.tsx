529a5419209cd0317ac27b1e38dc97a05efda565
nayeong lee <lny021102@gmail.com>
2025-01-10 05:08:16 +0900

    feat: router 파일에 게시물 작성, 템플릿 등록 경로 추가

--- src/provider/route/Router.tsx
+++ src/provider/route/Router.tsx
@@ -1,38 +1,48 @@
 import { createBrowserRouter } from "react-router-dom";
 import RootLayout from "../../layout/RootLayout";
 import MainPage from "../../pages/main/MainPage";
+
 import MyPage from "../../pages/mine/MyPage";
+import WriteContentPage from "../../pages/mine/Write_Content_Page";
+import WriteTemplatePage from "../../pages/mine/Write_Template_Page";
 
 import Login from "../../pages/auth/Login";
 import TemplatePage from "../../pages/template/TemplatePage";
 import Register from "../../pages/auth/Register";
 
 export const router = createBrowserRouter([
-    {
-      path: '/',
-      element: <RootLayout />,
-      children: [
-        {
-          index: true,
-          element: <MainPage />,
-        },
-        {
-          path: 'my',
-          element: <MyPage />
-        },
-        {
-          path: 'template',
-          element: <TemplatePage />
-        },
-        {
-          path: '/login',
-          element: <Login/>
-        },
-        {
-          path: '/register',
-          element: <Register/>
-        }
-      ],
-    },
-  ]);
-  
+  {
+    path: '/',
+    element: <RootLayout />,
+    children: [
+      {
+        index: true,
+        element: <MainPage />,
+      },
+      {
+        path: 'my',
+        element: <MyPage />
+      },
+      {
+        path: 'template',
+        element: <TemplatePage />
+      },
+      {
+        path: '/login',
+        element: <Login />
+      },
+      {
+        path: '/register',
+        element: <Register />
+      },
+      {
+        path: '/writecontentpage',
+        element: <WriteContentPage />
+      },
+      {
+        path: '/writetemplatepage',
+        element: <WriteTemplatePage />
+      },
+    ],
+  },
+]);
