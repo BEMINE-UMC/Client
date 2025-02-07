491f62fe9d63de8d6cbc37e97753549476b132fe
nayeong lee <lny021102@gmail.com>
2025-02-08 00:11:01 +0900

    feat: 게시물 업로드 API 연동 성공. 다만 텍스트에디터 라이브러리 이미지 업로드 에러 해결 중

--- src/pages/mine/components/TextEditor.tsx
+++ src/pages/mine/components/TextEditor.tsx
@@ -1,28 +1,61 @@
-import { EditorState } from "draft-js";
+import { EditorState, convertToRaw } from "draft-js";
 import { Editor } from "react-draft-wysiwyg";
+import draftToHtml from "draftjs-to-html";
 import "react-draft-wysiwyg/dist/react-draft-wysiwyg.css";
 import "./TextEditor.css";
 
 interface TextEditorProps {
 	editorState: EditorState | null;
 	onEditorStateChange: (editorState: EditorState) => void;
+	onThumbnailSelect: (imageSrc: string) => void;
 }
 
-const TextEditor = ({ editorState, onEditorStateChange }: TextEditorProps) => {
+const TextEditor = ({ editorState, onEditorStateChange, onThumbnailSelect }: TextEditorProps) => {
 	const safeEditorState = editorState || EditorState.createEmpty();
 
 	const handleEditorChange = (newEditorState: EditorState) => {
 		onEditorStateChange(newEditorState);
 	};
 
+	const handleImageClick = (imageSrc: string) => {
+		if (window.confirm("이 이미지를 대표 이미지로 설정하시겠습니까?")) {
+			onThumbnailSelect(imageSrc);
+		}
+	};
+
+	const renderContentWithThumbnailButton = () => {
+		const rawContent = convertToRaw(safeEditorState.getCurrentContent());
+		const htmlContent = draftToHtml(rawContent);
+
+		const parser = new DOMParser();
+		const doc = parser.parseFromString(htmlContent, "text/html");
+		const images = doc.querySelectorAll("img");
+
+		images.forEach((image) => {
+			const button = document.createElement("button");
+			button.textContent = "대표 이미지로 선택";
+			button.style.marginLeft = "10px";
+			button.onclick = () => handleImageClick(image.src);
+			image.insertAdjacentElement("afterend", button);
+		});
+
+		return { __html: doc.body.innerHTML };
+	};
+
 	return (
-		<Editor
-			editorState={safeEditorState}
-			onEditorStateChange={handleEditorChange}
-			toolbarClassName="toolbarClassName"
-			wrapperClassName="wrapperClassName"
-			editorClassName="editorClassName"
-		/>
+		<div>
+			<Editor
+				editorState={safeEditorState}
+				onEditorStateChange={handleEditorChange}
+				toolbarClassName="toolbarClassName"
+				wrapperClassName="wrapperClassName"
+				editorClassName="editorClassName"
+			/>
+			<div
+				className="preview"
+				dangerouslySetInnerHTML={renderContentWithThumbnailButton()}
+			></div>
+		</div>
 	);
 };
 
