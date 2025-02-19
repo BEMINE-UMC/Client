cfe9e3241093dbe58d97adef5cfeba230b3d4498
Hyeonseo <cos122686@gmail.com>
2025-02-20 02:32:09 +0900

    충돌 해결

--- src/pages/main/MainPage.tsx
+++ src/pages/main/MainPage.tsx
@@ -52,15 +52,6 @@ export default MainPage;
 
 const PageContainer = styled.div`
     background: linear-gradient(to bottom, #ffffff, #fff6b4);
-
-    @media (max-width: 768px) {
-        width: min(100vw, 95%);
-    }
-
-    
-    @media (max-width: 480px) {
-        width: min(100vw, 95%); 
-    }
 `;
 
 const NoResultsContainer = styled.div`
