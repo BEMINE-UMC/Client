75762050adbf920c96ceda35ad161ad65f1ed3af
Hyeonseo <cos122686@gmail.com>
2025-02-20 02:20:51 +0900

    최신 develop 반영

--- src/pages/main/MainPage.tsx
+++ src/pages/main/MainPage.tsx
@@ -6,7 +6,7 @@ import useSearchStore from "../../store/search/searchStore";
 import SearchResults from "../../components/search/SearchResults";
 
 
-const MainPage = () => {
+const MainPage: React.FC = () => {
 
     const results = useSearchStore((state) => state.results);   //전역상태로 부터 검색결과 가져오기
     const searchTerm = useSearchStore((state) => state.searchTerm); // 전역상태로부터 검색어 가져오기
@@ -14,33 +14,31 @@ const MainPage = () => {
     console.log("🔎 MainPage 렌더링됨! 검색어:", searchTerm, "검색 결과 개수:", results.length);
     console.log("🟢 검색 결과 상태 확인:", results);
     return (
-        <PageContainer>
-            
-            {(results.length > 0 || searchTerm.trim()) ? (
-                <SearchResults searchResults={results} searchTerm={searchTerm}/>
-            ) : (
-                <Banner />
-            )}
-            <NoticeBoard />
-            
-        </PageContainer>
+        <MainContainer>
+            <ContentContainer>
+                {(results.length > 0 || searchTerm.trim()) ? (
+                    <SearchResults searchResults={results} searchTerm={searchTerm}/>
+                ) : (
+                    <Banner />
+                )}
+                <NoticeBoard />
+            </ContentContainer>
+        </MainContainer>
     );
 };
 
 export default MainPage;
 
-const PageContainer = styled.div`
-    position: absolute;
-    top: 0;
-    left: 0;
-    right: 0;
-    min-height: 100vh;
-    background: linear-gradient(to bottom, #ffffff, #fff6b4);
+const MainContainer = styled.div`
     display: flex;
-    justify-content: center;
-    align-items: center;
-    width: 100vw;
-    flex-direction: column;
-    margin: 0;
-    padding: 0;
+    flex-direction: column;  // 세로 방향 정렬
+    width: 100%;
+    min-height: 100vh;
+`;
+
+const ContentContainer = styled.div`
+    width: 100%;
+    max-width: 1920px;
+    margin: 0 auto;
+    padding: 20px;
 `;
